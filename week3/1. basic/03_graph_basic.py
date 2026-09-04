f5c8a14e094c556a19aaba94bd0f1cb4d9440496
Jihyun0511 <yjhjulia1@gmail.com>
2026-09-04 13:58:17 +0900

    w03_basic_03 edited

--- week3/1. basic/03_graph_basic.py
+++ week3/1. basic/03_graph_basic.py
@@ -47,15 +47,16 @@ def create_graph(vertices, edges, directed=False):
     # 초기화
     for v in range(vertices):
         graph[v] = []
+    # or you can just:
+    # graph = { v: [] for v in range(vertices)}
+    # 의식적으로 list comprehension을 사용하려고 해 봐야겠음...
     
-    # TODO: 간선 추가
-    ## 간선 추가 (u에서 v로)
-    ## 무방향 그래프면 반대 방향도 추가
-    for i, j in edges:
-        graph[i].append(j)
-
+    # 간선 추가 (u에서 v로)
+    for u, v in edges:
+        graph[u].append(v)
+        # 무방향 그래프면 반대 방향도 추가
         if not directed:
-            graph[j].append(i)
+            graph[u].append(v)
     
     return graph
 
