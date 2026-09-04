7d5821530bb0a63e978bc216570609409a5a480c
Jihyun0511 <yjhjulia1@gmail.com>
2026-09-04 22:45:03 +0900

    w3_basic_07 solved

--- week3/1. basic/07_dp_stairs.py
+++ week3/1. basic/07_dp_stairs.py
@@ -46,14 +46,20 @@ def climb_stairs(n):
         n번째 계단까지 오르는 방법의 수
     """
     # TODO: 특별한 경우 처리
-    pass
-    
+    if n<3: return n
     
     # TODO: dp 배열 생성 및 초기화
-    pass
+    # 얘는 딕셔너리로 안 쓰는 이유가...?
+    # 어차피 다 채워질 거라서? ㅇㅇ
+    dp = [0] * (n+1)
     
     # TODO: 작은 문제부터 차례로 계산
-    pass
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
     
     return dp[n]
 
